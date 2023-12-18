db8369b4bf7ce7e0316b0789ca7fcc1edf95dbfc
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2023-12-18 13:15:24 +0530

    Updated Solution - GfG to GitHub

--- Medium/Peak element/peak-element.cpp
+++ Medium/Peak element/peak-element.cpp
@@ -16,8 +16,13 @@ class Solution
     {
         if(arr[0] >= arr[1]) return 0;
         if(arr[n-1] >= arr[n-2]) return n-1;
-        for(int i = 1; i < n - 1; i++){
-           if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1]) return i;
+        int low = 0;
+        int high = n-1;
+        while(low <= high){
+            int mid = low + (high - low)/ 2;
+            if(arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1]) return mid;
+            else if(arr[mid] < arr[mid - 1]) high = mid - 1;
+            else low = mid + 1;
         }
         return -1;
     }
