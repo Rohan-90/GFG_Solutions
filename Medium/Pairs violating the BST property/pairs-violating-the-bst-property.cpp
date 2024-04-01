59cd8ef047fc588e13daa58c43c7bc740c0ab7e5
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2024-04-01 22:52:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Pairs violating the BST property/pairs-violating-the-bst-property.cpp
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Tree node
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Utility function to create a new node
+Node* newNode(int data) {
+    Node* temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return (temp);
+}
+
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*// A Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};*/
+
+class Solution {
+  public:
+
+ int k = 0; // Global variable for indexing during in-order traversal
+
+void inorder(Node* root, vector<int>& arr) {
+    if (root == nullptr) {
+        return;
+    }
+    inorder(root->left, arr);
+    arr[k++] = root->data; // Place the current node's value in the array
+    inorder(root->right, arr);
+}
+
+int countAndMerge(vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    vector<int> L(n1), R(n2);
+    
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    
+    int i = 0, j = 0, k = l, inversions = 0;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+            inversions += (n1 - i); // Count inversions here
+        }
+    }
+    
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+    
+    return inversions;
+}
+
+int countInversion(vector<int>& arr, int l, int r) {
+    if (l >= r) return 0;
+    int mid = l + (r - l) / 2;
+    int inv = countInversion(arr, l, mid);
+    inv += countInversion(arr, mid + 1, r);
+    inv += countAndMerge(arr, l, mid, r);
+    return inv;
+}
+
+int pairsViolatingBST(int n,Node* root) {
+    k = 0; // Reset k before each call
+    vector<int> arr(n); // Using vector for dynamic array
+    inorder(root, arr); // Fill the array using in-order traversal
+    
+    return countInversion(arr, 0, n - 1);
+}
+
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    getchar();
+
+    while (t--) {
+
+        int n;
+        cin >> n;
+        getchar();
+
+        string inp;
+        getline(cin, inp);
+
+        struct Node* root = buildTree(inp);
+
+        Solution ob;
+        int ans = ob.pairsViolatingBST(n, root);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
