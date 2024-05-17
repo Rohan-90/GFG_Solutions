15bbae1fd1ac5e2d3bd212981ee0312ab9c61a6c
Rohan Banerjee <130914496+Rohan-90@users.noreply.github.com>
2024-05-17 11:28:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Pair Given Difference/find-pair-given-difference.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findPair(int n, int x, vector<int> &arr) {
+           unordered_set<int> seen;
+    
+    for (int i = 0; i < n; i++) {
+        int current = arr[i];
+        
+        // Check if there is an element in the set such that the absolute difference is x
+        if (seen.find(current + x) != seen.end() || seen.find(current - x) != seen.end()) {
+            return 1;
+        }
+        
+        // Add the current element to the set
+        seen.insert(current);
+    }
+    
+    return -1;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        int x;
+        scanf("%d", &x);
+
+        vector<int> arr(n);
+        Array::input(arr, n);
+
+        Solution obj;
+        int res = obj.findPair(n, x, arr);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
